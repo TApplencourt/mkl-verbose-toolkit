7380153662a3825fe8c54781bc2094de097665af
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2018-08-03 18:21:26 -0500

    Update mkl_hook.sh

--- mkl_hook.sh
+++ mkl_hook.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PLOCK=~/myscript.lock
 #Export MKL_VERBOSE for one rank
 
-# If ALPS_APP_PE variable is set and equal 1 or if I can create the directory source 
+PLOCK=~/mkl_verbose.$$.lock
+# If ALPS_APP_PE variable is set and equal 1 or if I can take the lock, export the envirement
+#ALPS_APP_PE Cray special variable
 if ( [ -z "$ALPS_APP_PE" ] && mkdir "$PLOCK" 2>/dev/null ) || [ "${ALPS_APP_PE:-0}" -eq 1 ]; then
     export MKL_VERBOSE=1
 fi
@@ -17,8 +18,6 @@ if [ -z "$ALPS_APP_PE" ]; then
  trap cleanup EXIT SIGTERM
 fi
 #Run the command. 
-#Take a nap before in order to avoid race condition
+#Take a nap before in order to avoid race condition (removal of the lock before the start of some process)
 sleep 1
 "$@"
-
-
